e2ccf8ef3aec5b3abf1d1d3c093fede2f151f42a
Jiayuli-CU <jiayu.li@columbia.edu>
2021-05-19 20:12:33 +0800

    v1.0

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2021 Online StarLink Tracker. All Rights Reserved. Website Made by Jiayu Li
+            </footer>
+        );
+    }
+}
+
+export default Footer;
